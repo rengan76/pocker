a38d3994acb2da5e5a9a2ed171849e1eb60ab476
rramanujam <rramanujam@efi.org>
2018-10-07 09:30:44 -0400

    Added test case with mocha

---
+++ ranktest.js
@@ -0,0 +1,18 @@
+var assert = require("assert");
+
+
+//A♥K♣7♠k♠2♥
+
+function rank(str) {
+    return '1 Pair';
+} 
+describe('Testing for finding Rank', function() {
+   
+   it('should return string 1 Pair', function() {
+       var expected = rank("A♥K♣7♠k♠2♥")
+	    assert( expected  == '1 Pair', 'return string');
+	   });
+	   
+	  
+	   
+});
